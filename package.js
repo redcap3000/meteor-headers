e41eb69d0027bb77ac1810597ff8b0f6cdab2f12
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2015-04-17 08:19:24 -0800

    fixes to package file

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'redcap3000:server_headers',
   summary: 'Access HTTP headers on server only',
   version: "0.0.1",
-  git: 'https://github.com/redcap3000/meteor-server_headers.git'
+  git: 'https://github.com/redcap3000/meteor-headers.git'
 });
 
 Npm.depends({
@@ -11,9 +11,9 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom("0.9.0");
-  api.use(['webapp', 'livedata', 'templating', 'deps'], ['client', 'server']);
+  api.use(['webapp', 'livedata', 'templating', 'deps'], 'server');
   api.use('appcache', 'server', { weak: true });
-  api.use("meteorhacks:inject-initial@1.0.2", ['server', 'client']);
+  api.use("meteorhacks:inject-initial@1.0.2", 'server');
 
   api.addFiles('headers-common.js',  'server');
   api.addFiles('headers-server.js', 'server');
